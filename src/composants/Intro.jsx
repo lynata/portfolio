5fe3468dca0c5ea59a56decc225b7e5d4ddac557
Lyna <lyly8888@hotmail.com>
2021-03-07 14:54:39 -0500

    intro complete

---
+++ src/composants/Intro.jsx
@@ -0,0 +1,22 @@
+import './Intro.scss';
+import imgLyna from '../images/me.jpg';
+import point from '../images/point.png';
+import point2 from '../images/point2.png';
+
+export default function Intro() {
+    return(
+        <section className="Intro">
+            <div className="avatar">
+                <h3>Lyna Tang</h3>
+                <img id="imgLyna" src={imgLyna} alt="Lyna"/>
+            </div>
+            <div className="desc">
+                <ul>
+                    <li><img className="fleur" src={point} alt=""/> Developpeuse front-end</li>
+                    <li><img className="fleur" src={point2} alt=""/> Designeuse</li>
+                    <li><img className="fleur" src={point} alt=""/> Illustratrice</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
--- src/composants/Page.jsx
+++ src/composants/Page.jsx
@@ -1,10 +1,12 @@
 import './Page.scss';
 import Nav from './Nav';
+import Intro from './Intro';
 
 export default function Page() {
   return (
     <div className="Page">
       <Nav/>
+      <Intro/>
     </div>
   );
 }
